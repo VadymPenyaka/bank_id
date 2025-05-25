ee10b7dda809873961308ce5a93127616269d273
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2025-05-25 16:01:54 +0300

    refactor PersonResolver

--- src/main/java/peniaka/bankid/resolver/PersonResolver.java
+++ src/main/java/peniaka/bankid/resolver/PersonResolver.java
@@ -4,16 +4,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import peniaka.bankid.exception.NotFoundException;
 import peniaka.bankid.model.PersonDTO;
 import peniaka.bankid.service.PersonService;
 
 import java.util.UUID;
 @Controller
+@RequestMapping(PersonResolver.BASE_PATH)
 @RequiredArgsConstructor
 public class PersonResolver {
     private final PersonService personService;
+    public static final String BASE_PATH = "/api/v1/person";
 
     @QueryMapping
     public PersonDTO person (@Argument UUID id) {
@@ -22,7 +27,8 @@ public class PersonResolver {
     }
 
     @MutationMapping
-    public PersonDTO createPerson(@Argument("person") PersonDTO person) {
-        return personService.createPerson(person);
+    public ResponseEntity<?> createPerson(@Argument("person") PersonDTO person) {
+        personService.createPerson(person);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
